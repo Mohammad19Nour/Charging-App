43a7eff6e2acaaea5f6d36b3598dd4e9cc7c3212
Mohammad19Nour <mohammad19nour@gmail.com>
2022-12-22 08:01:33 +0300

    g

--- ChargingApp/Entity/ChangerAndCompany.cs
+++ ChargingApp/Entity/ChangerAndCompany.cs
@@ -5,7 +5,6 @@ public class ChangerAndCompany
 {
     public int Id { get; set; }
     public RechargeMethod RechargeMethodMethod { get; set; }
-    public int PaymentMethodId { get; set; }
     public string ArabicName { get; set; }
     public string EnglishName { get; set; }
 }
