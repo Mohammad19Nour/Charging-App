2fb2bfa8545fbfd97031d0c0f08b50291bce3445
Mohammad19Nour <mohammad19nour@gmail.com>
2023-01-09 17:50:26 +0300

    add debit for user

--- ChargingApp/DTOs/NewPaymentDto.cs
+++ ChargingApp/DTOs/NewPaymentDto.cs
@@ -5,19 +5,17 @@ namespace ChargingApp.DTOs;
 public class NewPaymentDto
 {
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
-    public string? Notes{ get; set; }
-  [Required]  public double AddedValue { get; set; }
-    
-   [Required] public IFormFile ImageFile { get; set; }
-    
-    
+    public string? Notes { get; set; }
+    [Required] public double AddedValue { get; set; }
+    [Required] public IFormFile ImageFile { get; set; }
 }
 
 public class NewCompanyPaymentDto : NewPaymentDto
-{ 
+{
     public string? Username { get; set; }
 }
+
 public class NewOfficePaymentDto : NewPaymentDto
-{ 
+{
     public string? Username { get; set; }
 }
